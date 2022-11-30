780f6be3646b94bf78d3c8ba6f50329b6c4597da
James Hart <25505178+HartJN@users.noreply.github.com>
2022-12-01 10:17:57 +1300

    add more seeds

--- server/db/seeds/host.mjs
+++ server/db/seeds/host.mjs
@@ -5,5 +5,7 @@ export const seed = async (knex) => {
   // Inserts seed entries
   await knex('host').insert([
     { host_id: 1, auth0id: 'fshueisges93942', name: 'Suzanne' },
+    { host_id: 2, auth0id: '93943fshueisges', name: 'Bob' },
+    { host_id: 3, auth0id: '93943fshueisges', name: 'Sally' },
   ])
 }
