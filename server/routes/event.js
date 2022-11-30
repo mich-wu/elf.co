a13bb561a3a78e49d4a70e6687ef3c671050faae
James Hart <25505178+HartJN@users.noreply.github.com>
2022-12-01 10:11:26 +1300

    add event routes

--- server/routes/event.js
+++ server/routes/event.js
@@ -1,8 +1,27 @@
 import express from 'express'
 const router = express.Router()
 
-router.get('/', (req, res) => {
-  res.json({ message: 'Hello Event' })
+import { createEvent, getEvent, getEvents } from '../db/functions/events.js'
+
+router.get('/dashboard', async (req, res) => {
+  const events = await getEvents()
+  res.send(events)
+})
+
+router.get('/dashboard/:event_id', async (req, res) => {
+  const { event_id } = req.params
+  const event = await getEvent(event_id)
+  res.send(event)
+})
+
+router.post('/', async (req, res) => {
+  console.log('hit post')
+  try {
+    const newEvent = await createEvent(req.body)
+    res.status(200).json(newEvent)
+  } catch (err) {
+    res.status(500).json({ error: err.message })
+  }
 })
 
 export default router
