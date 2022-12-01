a65cb9084b5f979ae768a70dea300162bf8ab4bb
Manuela <111711683+Manuela-Ovalle@users.noreply.github.com>
2022-12-02 12:35:46 +1300

    ingridient list showing

--- client/apiClient/drinks.js
+++ client/apiClient/drinks.js
@@ -2,7 +2,6 @@ import request from 'superagent'
 
 export function getRandomDrink() {
   return request.get(`/api/v1/drinks`).then((response) => {
-    console.log('drinksapi:', response.body.drinks[0])
     return response.body.drinks[0]
   })
 }
--- client/pages/Drinks.jsx
+++ client/pages/Drinks.jsx
@@ -3,7 +3,8 @@ import { useEffect, useState } from 'react'
 import { getRandomDrink } from '../apiClient/drinks.js'
 
 const Drinks = () => {
-  const [drink, setDrink] = useState([])
+  const [drink, setDrink] = useState({})
+  const [ingredients, setIngredients] = useState([])
 
   useEffect(() => {
     getRandomDrink()
@@ -15,15 +16,35 @@ const Drinks = () => {
       })
   }, [])
 
+  useEffect(() => {
+    const allIngredients = Object.fromEntries(
+      Object.entries(drink).filter(([key]) => key.includes('strIngredient'))
+    )
+
+    const ingredientsArray = Object.values(allIngredients).filter(
+      (ingredient) => ingredient !== null
+    )
+
+    setIngredients(ingredientsArray)
+    console.log(ingredientsArray)
+  }, [drink])
+
   return (
     <>
       <div>
         <h1>{drink?.strDrink}</h1>
-        <img src={drink?.strDrinkThumb} alt={drink?.srtDrink}></img>
-        {/* <p>Ingredients: {drink.ingredients}</p> */}
-        <p>Instructions: {drink?.strInstructions}</p>
+        <img src={drink?.strDrinkThumb} width='600' alt={drink?.srtDrink}></img>
+        <p>Ingredients:</p>
+        {ingredients?.length > 0 && (
+          <ul>
+            {ingredients?.map((ingredient, key) => {
+              return <li key={key}>{ingredient}</li>
+            })}
+          </ul>
+        )}
         <p>Glass type: {drink?.strGlass}</p>
         <p>Category: {drink?.strCategory}</p>
+        <p>Instructions: {drink?.strInstructions}</p>
       </div>
     </>
   )
