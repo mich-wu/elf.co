aabf8f77e32eb40e49f94a9ac78feefe7dda674a
James Hart <25505178+HartJN@users.noreply.github.com>
2022-12-02 05:37:18 +1300

    rename db wishlist to guest

--- server/db/functions/guest.js
+++ server/db/functions/guest.js
@@ -1,21 +1,21 @@
 import connection from '../connection.js'
 
 export function getWishlist(db = connection) {
-  return db('wishlist').select()
+  return db('guest').select()
 }
 
 export function getWishlistById(id, db = connection) {
-  return db('wishlist').select().where('guest_code', id)
+  return db('guest').select().where('guest_code', id)
 }
 
 export function createWishlist(wish, db = connection) {
-  return db('wishlist').insert(wish)
+  return db('guest').insert(wish)
 }
 
 export function updatedWishlist(id, wish, db = connection) {
-  return db('wishlist').where('guest_code', id).update(wish)
+  return db('guest').where('guest_code', id).update(wish)
 }
 
 export function deleteWishlist(id, db = connection) {
-  return db('wishlist').del().where('id', id)
+  return db('guest').del().where('id', id)
 }
