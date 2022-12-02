d7316eeb3cd5128dc9ff5c7d2839cee9d07daeed
Manuela <111711683+Manuela-Ovalle@users.noreply.github.com>
2022-12-02 14:46:29 +1300

    measurement and ingredients working!!

--- client/pages/Drinks.jsx
+++ client/pages/Drinks.jsx
@@ -21,11 +21,9 @@ const Drinks = () => {
     const allIngredients = Object.fromEntries(
       Object.entries(drink).filter(([key]) => key.includes('strIngredient'))
     )
-
     const ingredientsArray = Object.values(allIngredients).filter(
       (ingredient) => ingredient !== null
     )
-
     setIngredients(ingredientsArray)
   }, [drink])
 
@@ -39,41 +37,19 @@ const Drinks = () => {
     setMeasures(measuresArray)
   }, [drink])
 
-  const map1 = new Map([
-    [
-      ingredients?.length > 0 && (
-        <ul>
-          {ingredients?.map((ingredient, key) => {
-            return <li key={key}>{ingredient}</li>
-          })}
-        </ul>
-      ),
-    ],
-  ])
-  const map2 = new Map([
-    [
-      measures?.length > 0 && (
-        <ul>
-          {measures?.map((measure, key) => {
-            return <li key={key}>{measure}</li>
-          })}
-        </ul>
-      ),
-    ],
-  ])
-
-  const map3 = new Map([...map2], [...map1])
-  console.log(map3)
-
   return (
     <>
       <div>
         <h1>{drink?.strDrink}</h1>
         <img src={drink?.strDrinkThumb} width='600' alt={drink?.srtDrink}></img>
         <p>Ingredients:</p>
-        <li>
-          {map2} {map1}
-        </li>
+        {ingredients?.map((ingredient, key) => {
+          return (
+            <li key={key}>
+              {measures[key] || 'Add'} {ingredient}
+            </li>
+          )
+        })}
         <p>Glass type: {drink?.strGlass}</p>
         <p>Category: {drink?.strCategory}</p>
         <p>Instructions: {drink?.strInstructions}</p>
