51b0e73d801a89a1caaa5cbbf2772c6791b3eeab
Manuela <111711683+Manuela-Ovalle@users.noreply.github.com>
2022-12-02 13:15:27 +1300

    map function

--- client/pages/Drinks.jsx
+++ client/pages/Drinks.jsx
@@ -5,6 +5,7 @@ import { getRandomDrink } from '../apiClient/drinks.js'
 const Drinks = () => {
   const [drink, setDrink] = useState({})
   const [ingredients, setIngredients] = useState([])
+  const [measures, setMeasures] = useState([])
 
   useEffect(() => {
     getRandomDrink()
@@ -26,8 +27,43 @@ const Drinks = () => {
     )
 
     setIngredients(ingredientsArray)
-    console.log(ingredientsArray)
   }, [drink])
+
+  useEffect(() => {
+    const allMeasures = Object.fromEntries(
+      Object.entries(drink).filter(([key]) => key.includes('strMeasure'))
+    )
+    const measuresArray = Object.values(allMeasures).filter(
+      (measure) => measure !== null
+    )
+    setMeasures(measuresArray)
+  }, [drink])
+
+  const map1 = new Map([
+    [
+      ingredients?.length > 0 && (
+        <ul>
+          {ingredients?.map((ingredient, key) => {
+            return <li key={key}>{ingredient}</li>
+          })}
+        </ul>
+      ),
+    ],
+  ])
+  const map2 = new Map([
+    [
+      measures?.length > 0 && (
+        <ul>
+          {measures?.map((measure, key) => {
+            return <li key={key}>{measure}</li>
+          })}
+        </ul>
+      ),
+    ],
+  ])
+
+  const map3 = new Map([...map2], [...map1])
+  console.log(map3)
 
   return (
     <>
@@ -35,13 +71,9 @@ const Drinks = () => {
         <h1>{drink?.strDrink}</h1>
         <img src={drink?.strDrinkThumb} width='600' alt={drink?.srtDrink}></img>
         <p>Ingredients:</p>
-        {ingredients?.length > 0 && (
-          <ul>
-            {ingredients?.map((ingredient, key) => {
-              return <li key={key}>{ingredient}</li>
-            })}
-          </ul>
-        )}
+        <li>
+          {map2} {map1}
+        </li>
         <p>Glass type: {drink?.strGlass}</p>
         <p>Category: {drink?.strCategory}</p>
         <p>Instructions: {drink?.strInstructions}</p>
