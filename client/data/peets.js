b90033282bdbbc51cd2ded833ea8402d639dd83a
Cameron Louw <70700727+cameron-louw@users.noreply.github.com>
2022-12-02 13:06:36 +1300

    gallery

---
+++ client/data/peets.js
@@ -0,0 +1,9 @@
+export default [
+  {
+    id: 1,
+    owner: 'Eleanor',
+    petname: 'Sandy',
+    image:
+      'https://cdn.discordapp.com/attachments/1025349430469218314/1048011696612065381/20221202_112310.jpg',
+  },
+]
---
+++ client/pages/GalleryPage.jsx
@@ -0,0 +1,28 @@
+import React, { useEffect, useState } from 'react'
+
+import peets from '../data/peets'
+import styles from './Gallery.module.css'
+
+export default function GalleryPage() {
+  return (
+    <>
+      <h1> Peets 🐾 </h1>
+
+      <div className={styles['wrapper']}>
+        <div className='item'>
+          {peets.map((peet) => {
+            return (
+              <div key={peet.id} className={styles['polaroid']}>
+                <img src={peet.image} alt='dog' />
+                <div className={styles['caption']}>
+                  <p>{peet.owner}</p>
+                  <p>{peet.petname}</p>
+                </div>
+              </div>
+            )
+          })}
+        </div>
+      </div>
+    </>
+  )
+}
