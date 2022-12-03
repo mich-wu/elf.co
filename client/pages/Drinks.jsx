4ffbbbff7142406eef1cc348f4884d100f2f31de
Michelle Wu <111860906+mich-wu@users.noreply.github.com>
2022-12-03 17:14:31 +1300

    loading spinner working for drinks

--- client/pages/Drinks.jsx
+++ client/pages/Drinks.jsx
@@ -1,11 +1,11 @@
 import { CSSProperties, useEffect, useMemo, useState } from 'react'
 import { Link } from 'react-router-dom'
-import ClipLoader from 'react-spinners/ClipLoader'
+import ClipLoader from 'react-spinners/SyncLoader'
 
 const override = {
   display: 'block',
   margin: '0 auto',
-  borderColor: 'red',
+  borderColor: '#eac3ff',
 }
 
 import { getRandomDrink } from '../apiClient/drinks.js'
@@ -13,7 +13,7 @@ import { getRandomDrink } from '../apiClient/drinks.js'
 const Drinks = () => {
   const [drink, setDrink] = useState({})
   const [loading, setLoading] = useState(true)
-  let [color, setColor] = useState('#ffffff')
+  const [showText, setShowText] = useState(false)
 
   useEffect(() => {
     getRandomDrink()
@@ -45,43 +45,50 @@ const Drinks = () => {
     return measuresArray
   }, [drink])
 
+  const handleLoad = () => {
+    setLoading(false)
+    setShowText(true)
+  }
+
   return (
     <>
       <div className='sweet-loading'>
-        <button onClick={() => setLoading(!loading)}>Toggle Loader</button>
-        <input
-          value={color}
-          onChange={(input) => setColor(input.target.value)}
-          placeholder='Color of the loader'
-        />
-
         <ClipLoader
-          color={color}
+          color='#eac3ff'
           loading={loading}
           cssOverride={override}
-          size={150}
+          size={25}
           aria-label='Loading Spinner'
           data-testid='loader'
         />
       </div>
 
       <div>
-        <h1>{drink?.strDrink}</h1>
-        <img src={drink?.strDrinkThumb} width='600' alt={drink?.srtDrink}></img>
-        <p>Ingredients:</p>
-        {ingredients?.map((ingredient, key) => {
-          return (
-            <li key={key}>
-              {measures[key] || 'Add'} {ingredient}
-            </li>
-          )
-        })}
-        <p>Glass type: {drink?.strGlass}</p>
-        <p>Category: {drink?.strCategory}</p>
-        <p>Instructions: {drink?.strInstructions}</p>
-      </div>
-      <div>
-        <Link to='/'>Go Home</Link>
+        <img
+          src={drink?.strDrinkThumb}
+          width='600'
+          alt={drink?.srtDrink}
+          onLoad={handleLoad}
+        ></img>
+        {showText ? (
+          <>
+            <h1>{drink?.strDrink}</h1>
+            <p>Ingredients:</p>
+            {ingredients?.map((ingredient, key) => {
+              return (
+                <li key={key}>
+                  {measures[key] || 'Add'} {ingredient}
+                </li>
+              )
+            })}
+            <p>Glass type: {drink?.strGlass}</p>
+            <p>Category: {drink?.strCategory}</p>
+            <p>Instructions: {drink?.strInstructions}</p>
+            <div>
+              <Link to='/'>Go Home</Link>
+            </div>
+          </>
+        ) : null}
       </div>
     </>
   )
