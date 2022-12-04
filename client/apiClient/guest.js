e36aff820d36a4159968399aff46a3f9120d300f
James Hart <25505178+HartJN@users.noreply.github.com>
2022-12-05 06:59:16 +1300

    getbyguestcode api call

--- client/apiClient/guest.js
+++ client/apiClient/guest.js
@@ -7,6 +7,14 @@ export function getWishlistApi() {
   })
 }
 
+export function getEventByGuestCodeApi(guest_code) {
+  console.log('hit getEventByGuestCodeApi', guest_code)
+  return request.get(`${wishlistURL}/${guest_code}/event`).then((res) => {
+    console.log('res.body in api call', res.body)
+    return res.body
+  })
+}
+
 export function getWishlistByIdApi(id) {
   return request.get(`${wishlistURL}/${id}`).then((res) => {
     return res.body
@@ -43,4 +51,12 @@ export const createGuestApi = async (newGuest) => {
   const res = await request.post('/api/v1/wishlist', newGuest)
 
   return res.body[0]
+}
+
+export const getAssignedWishlist = async (guest_code) => {
+  console.log('hit getAssignedWishlist', guest_code)
+  const res = await request.get(`/api/v1/wishlist/${guest_code}/assigned`)
+  console.log('res.body in getAssignedWishlist', res.body)
+
+  return res.body
 }
