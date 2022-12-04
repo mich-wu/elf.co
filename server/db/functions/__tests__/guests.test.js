4630d91d32baf921c9a265b5dc7bc6c97a046517
Michelle Wu <111860906+mich-wu@users.noreply.github.com>
2022-12-04 17:29:21 +1300

    pass getWishlist db test

--- server/db/functions/__tests__/guests.test.js
+++ server/db/functions/__tests__/guests.test.js
@@ -1,17 +1,44 @@
 import knex from 'knex'
 
-import config from '../../../../knexfile.js'
-import { getWishlist } from '../guest.js'
-const testDb = knex(config)
+import config from '../../knexfile.js'
+const testDb = knex(config.test)
 
-beforeAll(() => testDb.migrate.latest())
-beforeEach(() => testDb.seed.run())
-afterAll(() => testDb.destroy())
+import { getWishlist } from '../guest.js'
+
+beforeAll(async () => {
+  await testDb.migrate.latest()
+})
+
+beforeEach(async () => {
+  await testDb.seed.run()
+})
+
+afterAll(async () => {
+  await testDb.destroy()
+})
 
 describe('getWishlist', () => {
-  it('gets the guest table', () => {
+  it('gets the guest table from the database', () => {
+    return getWishlist(testDb).then((guest) => {
+      expect(guest).toHaveLength(4)
+    })
+  })
+
+  it('shows the guest name', () => {
     return getWishlist(testDb).then((guest) => {
       expect(guest[0].name).toBe('Bruno')
+    })
+  })
+
+  it('shows the guests wishlist', () => {
+    return getWishlist(testDb).then((guest) => {
+      expect(guest[1].wishlist).toBe('BBQ and a new lawnmower')
+    })
+  })
+
+  it('shows the guests guest-code', () => {
+    return getWishlist(testDb).then((guest) => {
+      expect(guest[2].guest_code).toBe('1D9C774D326AD157D6F889ACE9A7DA10')
     })
   })
 })
