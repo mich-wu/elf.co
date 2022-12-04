a0088d855409284a511ca8df2053af248a3464c7
James Hart <25505178+HartJN@users.noreply.github.com>
2022-12-05 07:03:14 +1300

    move assign logic to controller file

--- server/routes/event.js
+++ server/routes/event.js
@@ -1,8 +1,6 @@
 import express from 'express'
 
 import eventController from '../controllers/event.js'
-import { getGuestsByEventId } from '../db/functions/events.js'
-import { updateWishlistGifter } from '../db/functions/guest.js'
 
 const router = express.Router()
 
@@ -10,49 +8,6 @@ router.get('/dashboard', eventController.getEvents)
 router.post('/', eventController.createEvent)
 router.get('/dashboard/:event_id', eventController.getEvent)
 router.patch('/dashboard/:event_id', eventController.updateStatus)
-
-router.get('/dashboard/:event_id/assign', async (req, res) => {
-  const { event_id } = req.params
-
-  const event = await getGuestsByEventId(event_id)
-
-  const shuffle = (array) => {
-    for (let i = array.length - 1; i > 0; i--) {
-      const j = Math.floor(Math.random() * (i + 1))
-      ;[array[i], array[j]] = [array[j], array[i]]
-    }
-
-    return array
-  }
-
-  const assign = (array) => {
-    const shuffledArray = shuffle(array)
-    const assignments = shuffledArray.map((participant, i) => {
-      return {
-        id: participant.id,
-        gifter_id:
-          i === shuffledArray.length - 1
-            ? shuffledArray[0].id
-            : shuffledArray[i + 1].id,
-        guest_code: participant.guest_code,
-      }
-    })
-
-    return assignments
-  }
-
-  const assignments = assign(event)
-
-  const updateAssignments = async (assignments) => {
-    for (let i = 0; i < assignments.length; i++) {
-      await updateWishlistGifter(assignments[i])
-    }
-  }
-
-  updateAssignments(assignments)
-
-  const updatedEvent = await getGuestsByEventId(event_id)
-  res.json(updatedEvent)
-})
+router.get('/dashboard/:event_id/assign', eventController.assignGifters)
 
 export default router
