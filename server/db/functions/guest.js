41e9b27a650107e7c60b06c9b7e9fbda8acb3f50
James Hart <25505178+HartJN@users.noreply.github.com>
2022-12-05 07:06:42 +1300

    add wishlist logic

--- server/db/functions/guest.js
+++ server/db/functions/guest.js
@@ -24,3 +24,26 @@ export function updateWishlistGifter(assigned, db = connection) {
   const { gifter_id, guest_code, id } = assigned
   return db('guest').where({ guest_code }).update({ gifter_id })
 }
+
+export function getEventByInviteId(invite_id, db = connection) {
+  console.log(invite_id)
+  return db('event').where('invite_id', invite_id).first()
+}
+
+export function getWishListByGuestCode(guest_code, db = connection) {
+  console.log(guest_code, 'guest_code')
+  return db('guest').where('guest_code', guest_code).select()
+}
+
+export function getEventById(invite_id, db = connection) {
+  console.log(invite_id)
+  return db('event').where({ invite_id }).select()
+}
+
+export function getGuestByGiftId(gifter_id, db = connection) {
+  return db('guest').where('gifter_id', gifter_id).select('wishlist', 'name')
+}
+
+export function getById(id, db = connection) {
+  return db('guest').where('id', id).first()
+}
