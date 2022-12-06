d6462ffec007e51335f80e3daa4881b7ed1ec646
Cameron Louw <70700727+cameron-louw@users.noreply.github.com>
2022-12-06 18:32:10 +1300

    getWistById test in guest.test written but not passing

--- server/routes/__tests__/guest.test.js
+++ server/routes/__tests__/guest.test.js
@@ -12,23 +12,42 @@ beforeAll(() => connection.migrate.latest())
 beforeEach(() => connection.seed.run())
 afterAll(() => connection.destroy())
 
-// .get('/', guestController.getWishlist)
-// .get('/:id', guestController.getWishlistById)
-// .get('/:id/event', guestController.getEventById)
-// .get('/:guest_code/assigned', guestController.getWishListByGuestCode)
-// .post('/', guestController.createWishlist)
-// .patch('/:id', guestController.updatedWishlist)
-// .put('/:id', guestController.updateWishlistGifter)
-// .delete('/:id', guestController.deleteWishlist)
+// [x].get('/', guestController.getWishlist)
+// [ ].get('/:id', guestController.getWishlistById)
+// [ ].get('/:id/event', guestController.getEventById)
+// [ ].get('/:guest_code/assigned', guestController.getWishListByGuestCode)
+// [ ].post('/', guestController.createWishlist)
+// [ ].patch('/:id', guestController.updatedWishlist)
+// [ ].put('/:id', guestController.updateWishlistGifter)
+// [ ].delete('/:id', guestController.deleteWishlist)
 
 describe('GET /', () => {
   it('Renders the getWishlist db info', () => {
-    
-    return request (server)
-    .get('/api/v1/wishlist')
-    .then((res) => {
-      expect(res.status).toBe(200)
-    })
+    return request(server)
+      .get('/api/v1/wishlist')
+      .then((res) => {
+        expect(res.status).toBe(200)
+        console.log(res)
+      })
   })
 })
 
+describe('GET /:id', () => {
+  it('Gets wishilist by guest_code', () => {
+    const mockData = {
+      id: 0,
+      guest_code: '287DA10',
+      event_id: 1,
+      name: 'Bruno',
+      wishlist: 'gardening tools and soil',
+      gifter_id: expect.any(Number),
+    }
+
+    return request(server)
+      .get('/api/v1/wishlist/287DA10')
+      .then((res) => {
+        expect(res.status).toBe(200)
+        expect(res.body).toContain(mockData)
+      })
+  })
+})
