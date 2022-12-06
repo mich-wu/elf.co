3f4c5b9dd8d16e35f2dcd73ec6f41a97646ec294
Michaela <112312696+kala-kola@users.noreply.github.com>
2022-12-07 10:21:45 +1300

    test working

--- server/routes/__tests__/peets.test.js
+++ server/routes/__tests__/peets.test.js
@@ -4,31 +4,47 @@ import request from 'supertest'
 import * as db from '../../db/functions/peets.js'
 import server from '../../server.js'
 
+vi.spyOn(console, 'error').mockImplementation(() => {
+  return
+})
+vi.mock('../../db/functions/peets.js')
+
 beforeEach(() => {
   vi.resetAllMocks()
 })
 
 describe('GET/api/v1/peets', () => {
-  const mockData = [
-    {
-      id: 1,
-      petname: 'Croissant',
-      owner: 'Rohan',
-      image: 'PEET-18.jpg',
-    },
-    {
-      id: 2,
-      petname: 'Frank',
-      owner: 'Ben',
-      image: 'PEET-12.jpg',
-    },
-  ]
-  test.skip('returns an array of peets', () => {
+  test('returns an array of peets', () => {
+    const mockData = [
+      {
+        id: 1,
+        petname: 'Croissant',
+        owner: 'Rohan',
+        image: 'PEET-18.jpg',
+      },
+      {
+        id: 2,
+        petname: 'Frank',
+        owner: 'Ben',
+        image: 'PEET-12.jpg',
+      },
+    ]
     db.getPeets.mockReturnValue(Promise.resolve(mockData))
     return request(server)
       .get('/api/v1/peets')
       .then((res) => {
         expect(res.body).toHaveLength(2)
+        expect(res.status).toBe(200)
+      })
+  })
+  test('returns an error', () => {
+    db.getPeets.mockImplementation(() => Promise.reject('No Peets for you!'))
+
+    return request(server)
+      .get('/api/v1/peets')
+      .then((res) => {
+        expect(res.status).toBe(500)
+        expect(console.error).toHaveBeenCalledWith('No Peets for you!')
       })
   })
 })
