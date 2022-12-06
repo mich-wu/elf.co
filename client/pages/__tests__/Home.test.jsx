446b55d74372bfd9e122f0c3130ae8fad511df6a
eleanor-tosh <102724780+eleanor-tosh@users.noreply.github.com>
2022-12-06 18:12:58 +1300

    Home page component test

---
+++ client/pages/__tests__/Home.test.jsx
@@ -0,0 +1,44 @@
+import '@testing-library/jest-dom'
+
+import { render, screen } from '@testing-library/react'
+import React from 'react'
+import { MemoryRouter } from 'react-router-dom'
+import { describe, expect, it } from 'vitest'
+
+import Home from '../Home'
+
+describe('CreateEvent component test', async () => {
+  it('renders', () => {
+    render(<Home />, { wrapper: MemoryRouter })
+    expect(screen.getByText(/SECRET SANTA/i)).toBeInTheDocument()
+    expect(screen.getByText(/ONLYPEETS/i)).toBeInTheDocument()
+    expect(screen.getByText(/DRUNK SANTA/i)).toBeInTheDocument()
+  })
+  it('has images', () => {
+    render(<Home />, { wrapper: MemoryRouter })
+    expect(screen.getByAltText(/secret santa/i))
+    expect(screen.getByAltText(/Santa with Animals/i))
+    expect(screen.getByAltText(/Drunk Santa/i))
+  })
+  it('has a link to secret santa', async () => {
+    render(<Home />, { wrapper: MemoryRouter })
+    const link = await screen.findAllByRole('link', {
+      name: /SECRET SANTA/i,
+    })
+    expect(link[0].href).toContain('secretsanta')
+  })
+  it('has a link to onlypeets', async () => {
+    render(<Home />, { wrapper: MemoryRouter })
+    const link = await screen.findAllByRole('link', {
+      name: /ONLYPEETS/i,
+    })
+    expect(link[0].href).toContain('peets')
+  })
+  it('has a link to drunk santa', async () => {
+    render(<Home />, { wrapper: MemoryRouter })
+    const link = await screen.findAllByRole('link', {
+      name: /DRUNK SANTA/i,
+    })
+    expect(link[0].href).toContain('drinks')
+  })
+})
